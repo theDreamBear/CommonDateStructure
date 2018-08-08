dc05405b9b1cd0fc0bbdea960e2bf5609913e764
nic <512068167@qq.com>
2018-08-08 17:06:27 +0800

    first

---
+++ cmake-build-debug/CMakeFiles/commonDataStruecures.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/commonDataStruecures.dir/main.cpp.obj"
+  "commonDataStruecures.pdb"
+  "commonDataStruecures.exe"
+  "commonDataStruecures.exe.manifest"
+  "libcommonDataStruecures.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/commonDataStruecures.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
